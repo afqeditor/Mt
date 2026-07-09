bead164cdeb273dc31262c194a742faec80b9485
AFQ Editor <gamey0442@gmail.com>
2026-07-09 11:44:06 +0500

    Upgrade compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,13 +11,13 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 35
+  compileSdk = 36
 
 
   defaultConfig {
     applicationId = "com.aistudio.nexusmanager.vqyzn"
     minSdk = 26
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
